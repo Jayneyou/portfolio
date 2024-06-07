7e7ebbbc4fe64a5e09470620f94065da133b9369
Jayneyou <janeyoungsfw@gmail.com>
2024-06-07 16:44:35 -0400

    delete redundant folder

---
+++ src/components/navbar/Navbar.jsx
@@ -0,0 +1,25 @@
+import Sidebar from "../sidebar/Sidebar";
+import "./navbar.scss";
+import { motion } from "framer-motion";
+
+const Navbar = () => {
+    return (
+        <div className="navbar">
+            {/* side bar */}
+            <Sidebar />
+            {/* nav bar */}
+            <div className="wrapper">
+                <motion.span
+                    initial={{ opacity: 0, scale: 0.5 }}
+                    animate={{ opacity: 1, scale: 1 }}
+                    transition={{ duration: 0.5 }}>
+                    Jayne.dev</motion.span>
+                <div className="social">
+                    <a href="https://github.com/Jayneyou?tab=repositories"><img src="./github-icon.png" alt="GitHub" /></a>
+                    <a href="https://www.linkedin.com/in/jayne-you-346b24291/"><img src="./linkedin-icon.png" alt="LinkedIn" /></a>
+                </div>
+            </div>
+        </div>
+    )
+}
+export default Navbar;
---
+++ src/components/skills/Skills.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import Tab from "./Tab"
+import './skills.scss';
+
+
+const Skills = () => {
+
+    return (
+        <div className="skills">
+            <h3>My Skills</h3>
+            <Tab />
+        </div>
+    );
+};
+
+export default Skills;
